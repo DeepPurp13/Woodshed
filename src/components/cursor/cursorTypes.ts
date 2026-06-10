8c782595e2c021cd42961f1a4a41ceec14a6e14c
DeepPurp13 <59247947+DeepPurp13@users.noreply.github.com>
2026-06-09 21:54:04 -0500

    easy website i hope hehehe

--- src/components/cursor/cursorTypes.ts
+++
@@ -1,5 +0,0 @@
-export type CursorStackEntry =
-  | { type: "text"; variant: "default" | "permanent" | "accent"; text: string }
-  | { type: "image"; src: string; transparent: boolean }
-  | { type: "link" }
-  | { type: "opposite" };

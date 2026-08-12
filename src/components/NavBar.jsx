fc2e06fccc52cdb65aefb62a8cb2efdc7d56297b
DeepPurp13 <59247947+DeepPurp13@users.noreply.github.com>
2026-08-12 18:42:59 -0500

    footer mobile & ipad syyles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import "./Navstyling.css"
+import "./public/logo.png"
 
 
 function Navbar() {
   return (
     <header className="navbar">
-      <div className="navbar__title navbar__item">Moon Valley</div>
-      <div className="navbar__item link">About Us</div>
-      <div className="navbar__item link">Contact</div>
-      <div className="navbar__item link">Catalog</div>
+
+      <div className="navbar__title navbar__item"><a href="/"><img src="logo.png" /></a></div> 
+      <div className="navbar__item link"><a href="/about">About Us</a></div>
+      <div className="navbar__item link"><a href="/contact">Contact</a></div>
+      <div className="navbar__item link"><a href="/catalog">Catalog</a></div>
     </header>
   );
 }
